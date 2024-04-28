3b8fe81d58c63c3c0b0119ff0af7cb06ab7e70ba
aurelia12345 <aureliapragada@gmail.com>
2024-04-29 00:09:43 +0530

    removed unnecessary ns

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 
 from flask import Flask
-from api import ns, loc, dept, category, sub_category, user_ns
+from api import loc, dept, category, sub_category, user_ns
 from extensions import api, db
 
 
@@ -14,7 +14,6 @@ def create_app():
     api.init_app(app)
     db.init_app(app)
 
-    api.add_namespace(ns)
     api.add_namespace(loc)
     api.add_namespace(dept)
     api.add_namespace(category)
@@ -22,4 +21,9 @@ def create_app():
     api.add_namespace(user_ns)
 
     return app
+
+
+if __name__ == "__main__":
+    app = create_app()
+    app.run()
 
